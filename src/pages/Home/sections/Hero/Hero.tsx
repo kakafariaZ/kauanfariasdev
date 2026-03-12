4532e63ab53353e9135f2448d17a34672722dba0
Kauan Farias <arrudakauanfarias@gmail.com>
2026-03-11 23:38:30 -0300

    change: path pdf document

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -81,7 +81,7 @@ const Hero = () => {
                   display="flex"
                   justifyContent="center"
                 >
-                  <a href="/kauan_farias_cv_2026.pdf" download>
+                  <a href="/kauanfariasdev/kauan_farias_cv_2026.pdf" download>
                     <StyledButton onClick={() => console.log("curriculo baixado")}>
                       <DownloadIcon />
                       <Typography>Currículo</Typography>
